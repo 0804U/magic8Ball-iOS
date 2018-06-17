919921b90ca878a080515b2a0ed3a30e5e8fa3fe
Jeff Fumk <jfunk@mytech.com>
2018-06-16 19:42:51 -0500

    first commit

--- Magic 8-Ball/ViewController.swift
+++ Magic 8-Ball/ViewController.swift
@@ -9,7 +9,13 @@
 import UIKit
 
 class ViewController: UIViewController {
+    var randomBallShake : Int = 0
+    
+    @IBOutlet weak var ballImage1: UIImageView!
+    @IBOutlet var ballImage: UIView!
 
+    var ballArray = ["ball1", "ball2", "ball3", "ball4", "ball5"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +26,19 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func shakeActionButton(_ sender: UIButton) {
+        shakeBall()
+    }
+    
+    func shakeBall() {
+        randomBallShake = Int(arc4random_uniform(5))
+        
+        ballImage1.image = UIImage(named: ballArray[randomBallShake])
+    }
+    
+    override func motionEnded(_ motion: UIEventSubtype, with event: UIEvent?) {
+        shakeBall()
+    }
+    
 }
 
